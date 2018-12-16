524885dbf0d08e295a9c15ee789829b9bf9096e5
Merijn de Jonge <merijn.de.jonge@gmail.com>
2018-12-16 21:14:21 +0100

    Bug fix: only perform null checks for strings and non-value types

--- src/WebApiGenerator/CodeGenerator/Codegenerator.CreateMethodBody.cs
+++ src/WebApiGenerator/CodeGenerator/Codegenerator.CreateMethodBody.cs
@@ -20,6 +20,7 @@ namespace OpenSoftware.WebApiGenerator.CodeGenerator
             body = body.AddStatements(serviceCall);
             return body;
         }
+
         private static IEnumerable<StatementSyntax> CreateAssignmentStatements(MethodInfo methodInfo)
         {
             var varCount = 0;
@@ -29,7 +30,7 @@ namespace OpenSoftware.WebApiGenerator.CodeGenerator
                 if (attribute is FromHttpHeaderAttribute)
                 {
                     yield return HeaderAssignmentStatement(parameterInfo, varCount);
-                    yield return MakeValueCheck(varCount);
+                    yield return MakeValueCheck(parameterInfo, varCount);
                 }
                 else if (attribute is FromClaimAttribute)
                 {
@@ -39,6 +40,7 @@ namespace OpenSoftware.WebApiGenerator.CodeGenerator
                 {
                     continue;
                 }
+
                 varCount++;
             }
         }
@@ -63,6 +65,7 @@ namespace OpenSoftware.WebApiGenerator.CodeGenerator
                         {
                             propName = parameterInfo.Name;
                         }
+
                         parameterList.Add("payload." + propName);
                         break;
                     }
@@ -79,7 +82,8 @@ namespace OpenSoftware.WebApiGenerator.CodeGenerator
             }
             else
             {
-                callText = $"var result = _service.{methodInfo.Name}({string.Join(',', parameterList)}); return Ok(result);";
+                callText =
+                    $"var result = _service.{methodInfo.Name}({string.Join(',', parameterList)}); return Ok(result);";
             }
 
             callText = $"try {{ {callText} }} catch{{ return BadRequest(); }}";
@@ -88,8 +92,9 @@ namespace OpenSoftware.WebApiGenerator.CodeGenerator
 
         private static Attribute GetCustomAttribute(ParameterInfo parameterInfo)
         {
-            Attribute attribute = parameterInfo.GetCustomAttributes(typeof(FromHttpHeaderAttribute)).SingleOrDefault() as
-                FromHttpHeaderAttribute;
+            Attribute attribute =
+                parameterInfo.GetCustomAttributes(typeof(FromHttpHeaderAttribute)).SingleOrDefault() as
+                    FromHttpHeaderAttribute;
             if (attribute != null) return attribute;
             attribute =
                 parameterInfo.GetCustomAttributes(typeof(FromClaimAttribute)).SingleOrDefault() as FromClaimAttribute;
@@ -110,6 +115,7 @@ namespace OpenSoftware.WebApiGenerator.CodeGenerator
             {
                 headerName = parameterInfo.Name;
             }
+
             var statement =
                 SyntaxFactory.ParseStatement(
                     $@"var value{varCount} = GetFromHttpHeader<{type.FullName}>(""{headerName}"");");
@@ -125,16 +131,28 @@ namespace OpenSoftware.WebApiGenerator.CodeGenerator
             {
                 claimName = parameterInfo.Name;
             }
+
             var statement =
                 SyntaxFactory.ParseStatement(
                     $@"var value{varCount} = GetFromClaim<{type.FullName}>(""{claimName}"");");
             return statement;
         }
 
-        private static StatementSyntax MakeValueCheck(int varCount)
+        private static StatementSyntax MakeValueCheck(ParameterInfo parameterInfo, int varCount)
         {
-            return SyntaxFactory.ParseStatement(
-                $@"if(string.IsNullOrEmpty(value{varCount})){{ return BadRequest(); }} ");
+            if (parameterInfo.ParameterType == typeof(string))
+            {
+                return SyntaxFactory.ParseStatement(
+                    $@"if(string.IsNullOrEmpty(value{varCount})){{ return BadRequest(); }}");
+            }
+
+            if (parameterInfo.ParameterType.IsValueType == false)
+            {
+                return SyntaxFactory.ParseStatement(
+                    $@"if(value{varCount} == null){{ return BadRequest(); }}");
+            }
+
+            return SyntaxFactory.ParseStatement("");
         }
     }
 }
